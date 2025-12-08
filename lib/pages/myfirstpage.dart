43140f892a9bb1df510ab288d0e3e2ee01f78dfa
lad02_004 <6820230004@tsu.ac.th>
2025-12-08 12:08:44 +0700

    Add image profile

--- lib/pages/myfirstpage.dart
+++ lib/pages/myfirstpage.dart
@@ -8,16 +8,27 @@ class MyFirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       appBar: AppBar(
         title: Text("My Profile"),
         backgroundColor: const Color.fromARGB(255, 247, 175, 175),
       ),
-      body: Column(
-        children: [
-          Text("Patcharapon",style: TextStyle(fontSize: 22, color: Colors.white),),
-          Text("Boonpong",style: TextStyle(fontSize: 25, color: Colors.greenAccent),),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 100,
+            backgroundImage: AssetImage("assets/04-11-1024x683.jpg"),
+            ),
+            Icon(Icons.person),
+            Text("Patcharapon",style: TextStyle(fontSize: 22, color: Colors.white),),
+            Text("Boonpong",style: TextStyle(fontSize: 25, color: Colors.greenAccent),),
+            Text("6820230004",style: TextStyle(fontSize: 22, color: Colors.white),),
+            Text("6820230004@tsu.ac.th",style: TextStyle(fontSize: 25, color: Colors.greenAccent),),
+          ],
+        ),
       ),
       
     );
